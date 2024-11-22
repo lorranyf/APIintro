c1a91fe819a49cfcced9a2bb02662de88e96d1eb
lorranyf <lfss6@aluno.ifnmg.edu.br>
2024-11-22 23:39:15 +0000

    atividade14

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import express from "express";      // Requisição do pacote do express
+import express from "express";
+import dotenv from "dotenv";
+import { selectUsuarios } from "./bd.js";
+dotenv.config();
+
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
@@ -7,6 +11,16 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
     nome: "Lorrany_Fabia_santos_/_Lindha_Emanuelly_almeida",      // Substitua pelo seu nome
   });
+});
+
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
